6ee7d61aaa12afa8eb4794996277a3647a653cfb
Disha Sonkar <99535621+Disha-pro@users.noreply.github.com>
2026-09-19 13:29:58 +0530

    Create Abstraction.py

---
+++ Abstraction.py
@@ -0,0 +1,14 @@
+#Abstraction means hiding the complicated implementation and exposing only what the user needs to use.
+generate()
+   gem↓
+tokenization
+   ↓
+model processing
+   ↓
+attention layers
+   ↓
+probability calculation
+   ↓
+token generation
+   ↓
+response
